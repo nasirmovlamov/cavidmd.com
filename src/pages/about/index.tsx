37e174fad2fd7d16c02bc7ee328c68878ae48fa4
Nasir Movlamov <movlamovnasir2000@gmail.com>
2022-12-06 19:53:46 +0400

    fix: text-center

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,7 +5,7 @@ export const About = () => {
   return (
     <div className="pt-2 mt-24 sm:mt-24 md:mt-[270px] text-white mx-auto text-center max-w-[830px] px-10 ">
       <h1 className="text-3xl md:text-5xl text-[#B8FE00] font-bold">Who am I?</h1>
-      <p className="text-xs md:text-3xl mt-3 md:mt-10 md:leading-9 text-justify ">
+      <p className="text-center text-xs md:text-3xl mt-3 md:mt-10 md:leading-9 ">
         I'm a digital artist from Baku, Azerbaijan. I'm a CG Artist and Creator. 27 years old and have been in the
         design world for the last 10 years. I learned only graphic design for the first 3 years, then I turned to motion
         design and 3D art to improve myself. I specialize in 3D Arts and my artistic style is influenced by Cyberpunk
