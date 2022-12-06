36af0d2a4724140244f691b7b08793033932e56c
Nasir Movlamov <movlamovnasir2000@gmail.com>
2022-12-06 19:57:02 +0400

    fix: links added

--- src/components/shared/GamestopNft.tsx
+++ src/components/shared/GamestopNft.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const GamestopNft = () => {
   return (
-    <a href="#">
+    <a href="https://nft.gamestop.com/user/cavidmdw" target="_blank" rel="noreferrer">
       <div className="relative mx-4 w-[300px] sm:w-[652px] h-[187px] flex justify-center items-center overflow-hidden border-2 border-[#B8FE00] rounded-3xl bg-black">
         <img src={gameStopText} className="w-[279px] z-20" />
         <img src={gameStopBg} className="absolute w-full h-full object-cover hover:scale-110 transition-all" />
