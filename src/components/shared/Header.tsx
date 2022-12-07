7718b57dd8a114469299c7c994989c29166caca1
Nasir Movlamov <movlamovnasir2000@gmail.com>
2022-12-07 14:52:31 +0400

    fix : font everywhere

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -10,34 +10,34 @@ export const Header = () => {
   const { isMobileMenuOpen, setIsMobileMenuOpen } = useContext(GlobalContext);
 
   return (
-    <div className="justify-start xl:gap-12 md:justify-center md:w-max md:mx-auto flex gap-6 lg:gap-8 pt-8 text-xl text-white z-10 w-full mx-10 md:h-auto">
+    <header className="justify-start xl:gap-12 md:justify-center md:w-max md:mx-auto flex gap-6 lg:gap-8 pt-8 text-xl text-white z-10 w-full mx-10 md:h-auto ">
       <button onClick={() => setIsMobileMenuOpen(true)} className="md:hidden">
         <img src={mobileNav} className="w-5 h-5" alt="" />
       </button>
 
-      <Link to={"/artwork"} className={`${headerLinkChecker(location.pathname, "/artwork")} hidden md:flex`}>
+      <Link to={"/artwork"} className={`${headerLinkChecker(location.pathname, "/artwork")} hidden md:flex font-[b]`}>
         ARTWORK
       </Link>
-      <Link to={"/about"} className={`${headerLinkChecker(location.pathname, "/about")} hidden md:flex`}>
+      <Link to={"/about"} className={`${headerLinkChecker(location.pathname, "/about")} hidden md:flex font-[b]`}>
         ABOUT
       </Link>
-      <Link to={"/contact"} className={`${headerLinkChecker(location.pathname, "/contact")} hidden md:flex`}>
+      <Link to={"/contact"} className={`${headerLinkChecker(location.pathname, "/contact")} hidden md:flex font-[b]`}>
         CONTACT
       </Link>
 
       <Link to={"/"} className={"cursor-pointer mx-auto "}>
-        <img src={cavidMdLogo} alt="cavid mdw logo" className="w-[144px] h-[37px] lg:mx-24 -ml-10 md:ml-0" />
+        <img src={cavidMdLogo} alt="cavid mdw logo" className="w-[144px] h-[37px] lg:mx-24 -ml-10 md:ml-0 font-[b]" />
       </Link>
 
-      <Link to={"/nft"} className={`${headerLinkChecker(location.pathname, "/nft")} hidden md:flex`}>
+      <Link to={"/nft"} className={`${headerLinkChecker(location.pathname, "/nft")} hidden md:flex font-[b]`}>
         NFT
       </Link>
-      <Link to={"/asset"} className={`${headerLinkChecker(location.pathname, "/asset")} hidden md:flex`}>
+      <Link to={"/asset"} className={`${headerLinkChecker(location.pathname, "/asset")} hidden md:flex font-[b]`}>
         ASSET
       </Link>
-      <Link to={"/roadmap"} className={`${headerLinkChecker(location.pathname, "/roadmap")} hidden md:flex`}>
+      <Link to={"/roadmap"} className={`${headerLinkChecker(location.pathname, "/roadmap")} hidden md:flex font-[b]`}>
         ROADMAP
       </Link>
-    </div>
+    </header>
   );
 };
