9797e483a2df50f3f46b49538a4d11cde0b7dbda
Nasir Movlamov <movlamovnasir2000@gmail.com>
2022-12-08 15:00:24 +0400

    fix: img and video

--- src/components/shared/BackgroundVideo.tsx
+++ src/components/shared/BackgroundVideo.tsx
@@ -9,7 +9,7 @@ export const BackgroundVideo = () => {
       <>
         <iframe
           className="-z-20 w-screen h-screen bg-cover bg-center fixed top-0 left-0 "
-          src="https://www.youtube.com/embed/uiya0ecsgEY?controls=0&showinfo=0&playlist=uiya0ecsgEY&loop=1&autoplay=1&mute=1"
+          src="https://www.youtube.com/embed/qXBLWCP4rbA?controls=0&showinfo=0&playlist=uiya0ecsgEY&loop=1&autoplay=1&mute=1"
           allowFullScreen
         ></iframe>
         <div className="bg-[rgba(0,0,0,0.75)] fixed -z-10 w-screen h-screen top-0 left-0"></div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,14 +6,14 @@ import React from "react";
 export const Home = () => {
   return (
     <div className="mx-auto">
-      <div className="z-20 relative justify-center max-auto xl:max-w-7xl mt-[182px]  h-auto  2xl:w-[1582px] mx-auto hidden md:flex">
+      <div className="z-20 relative justify-center max-auto xl:max-w-7xl mt-[142px]  h-auto  2xl:w-[1582px] mx-auto hidden md:flex">
         <img
-          className="w-[1562px] h-[248px] absolute -z-10 mt-[150px] hover:translate-x-2 transition-all"
+          className="w-[1062px] h-[248px] absolute -z-10 mt-[150px] hover:translate-x-2 transition-all"
           src={maskTextElement}
           alt="cavidmd 3d"
         />
         <img
-          className="w-[508px] h-[605px] absolute -z-10 hover:-translate-x-2 transition-all"
+          className="w-[408px] h-[605px] absolute -z-10 hover:-translate-x-2 transition-all"
           src={homeElement}
           alt="cavidmd 3d"
         />
